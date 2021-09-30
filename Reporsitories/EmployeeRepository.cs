b60bd0deff0d170426eaa85b8ccac7e66a51f9cc
Bladimir Lora <bladimir.lora.solis@gmail.com>
2021-09-30 10:43:15 -0400

    Add project files.

---
+++ Reporsitories/EmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using WiredBrainCoffee.StorageApp.Entities;
+
+namespace WiredBrainCoffee.StorageApp.Reporsitories
+{
+    public class EmployeeRepository
+    {
+        public readonly List<Employee> _employees = new List<Employee>();
+        public void Add(Employee employee)
+        {
+
+        }
+    }
+}
